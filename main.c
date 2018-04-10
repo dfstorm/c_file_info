c12c7506c77a24a02cd4a99a0cca376611d2c419
dfstorm <gabriel.genois@gmail.com>
2018-04-10 15:02:00 +0200

    Ajout de la string de droit

--- main.c
+++ main.c
@@ -7,18 +7,45 @@
 #include <pwd.h> 		// user info
 #include <grp.h>		// Group info
 
+char	*build_perm_string(mode_t m)
+{
+	char	*dt;
+	
+	dt = ft_strnew(10);
+	dt[0] = (S_ISDIR(m) ? 'd' : '-');
+	dt[1] = (m & S_IRUSR ? 'r' : '-');
+	dt[2] = (m & S_IWUSR ? 'w' : '-');
+	dt[3] = (m & S_IXUSR ? 'x' : '-');
+	dt[4] = (m & S_IRGRP ? 'r' : '-');
+	dt[5] = (m & S_IWGRP ? 'w' : '-');
+	dt[6] = (m & S_IXGRP ? 'x' : '-');
+	dt[7] = (m & S_IROTH ? 'r' : '-');
+	dt[8] = (m & S_IWOTH ? 'w' : '-');
+	dt[9] = (m & S_IXOTH ? 'x' : '-');
+	return (dt);
+}
+
 int		main(int argc, char **argv)
 {
-	struct stat sb; 
-	struct passwd *pwd;
-	struct group *grp;
-	
-	if(argc || argv) {}
+	struct stat 	sb; 
+	struct passwd 	*pwd;
+	struct group 	*grp;
+	char 			*perm;
 
-	stat("test.txt", &sb);
+	if (argc < 2)
+	{
+		ft_printf("No file specified.\n");
+		return (0);
+	}
+	
+	if (stat(argv[1], &sb) < 0)
+	{
+		ft_printf("Invalid file\n");
+		return (0);
+	}
 
 	// nom
-	ft_printf("Nom: test.txt\n"); // Remplacer lors de listing de dir ... ?
+	ft_printf("Nom (+ maybe uri): %s\n", argv[1]);
 	
 	// Type
 	ft_printf("Type: ");
@@ -40,7 +67,9 @@ int		main(int argc, char **argv)
 		ft_printf("Inconue\n");
 
 	// Modes
-	ft_printf("Mode: %lo\n", sb.st_mode);
+	perm = build_perm_string(sb.st_mode);
+	ft_printf("Mode: %s\n", perm);
+	free(perm);
 	
 	// Nombre de liens
 	ft_printf("Nombre de lien: %i\n", sb.st_nlink);
@@ -57,7 +86,7 @@ int		main(int argc, char **argv)
 	ft_printf("Taille: %i octets \n", sb.st_size);
 	
 	// Date de derniere modif
-	ft_printf("st_ctime: %s\n", ctime(&sb.st_mtime));
+	ft_printf("Dernière modification: %s\n", ctime(&sb.st_mtime));
 	
 	return (0);
 }
